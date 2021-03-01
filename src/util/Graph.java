33859da896fd39de426203b9b2038df1d4718081
Tal Amar <f914gt29ra12@gmail.com>
2021-03-01 14:22:21 +0200

    fixed addVertex and added addEdge

--- src/util/Graph.java
+++ src/util/Graph.java
@@ -29,20 +29,64 @@ public class Graph<T, E> {
 
 	/* adds a vertex to the graph */
 	public boolean addVertex(Vertex<T> v) {
+		/* checks if the vertex is in the graph already */
+		/*-----------------------------------------------------------*/
+		for (int i = 0; i < vertex_list.size(); i++)
+			if (vertex_list.get(i).get(0).equals(v))
+				return false;
+		/*-----------------------------------------------------------*/
+
 		List<Vertex<T>> temp = new ArrayList<>();
 		temp.add(v);
-		System.out.println(temp.get(0).getKey());
-		if (vertex_list.contains(temp))
-			return false;
 		vertex_list.add(temp);
 		return true;
 	}
 
+	/* adds an edge to the graph and connects the vertices */
+	public boolean addEdge(Edge<T, E> e) {
+
+		/* checks if the edge is in the graph already */
+		/*-----------------------------------------------------------*/
+		if (edge_list.contains(e))
+			return false;
+		if (direction.equals(is_directed.NOT_DIRECTED)) { // checks if the edge is in the non-directed graph
+			for (int i = 0; i < vertex_list.size(); i++)
+				if (vertex_list.get(i).get(0).equals(e.getV1())) {
+					if (vertex_list.get(i).contains(e.getV2()))
+						return false;
+				} else if (vertex_list.get(i).get(0).equals(e.getV2()))
+					if (vertex_list.get(i).contains(e.getV1()))
+						return false;
+		} else // checks if the edge is in the directed graph
+			for (int i = 0; i < vertex_list.size(); i++)
+				if (vertex_list.get(i).get(0).equals(e.getV1()))
+					if (vertex_list.get(i).contains(e.getV2()))
+						return false;
+		/*-----------------------------------------------------------*/
+
+		if (direction.equals(is_directed.NOT_DIRECTED)) // adds a non-directed edge to the graph
+			for (int i = 0; i < vertex_list.size(); i++) {
+				if (vertex_list.get(i).get(0).equals(e.getV1()))
+					vertex_list.get(i).add(e.getV2());
+				if (vertex_list.get(i).get(0).equals(e.getV2()))
+					vertex_list.get(i).add(e.getV1());
+			}
+		else
+			for (int i = 0; i < vertex_list.size(); i++) // adds a directed edge to the graph
+				if (vertex_list.get(i).get(0).equals(e.getV1()))
+					vertex_list.get(i).add(e.getV2());
+		return true;
+	}
+
 	public static void main(String[] args) {
-		Graph<Integer, Integer> g = new Graph<Integer, Integer>(is_directed.NOT_DIRECTED);
-		Vertex<Integer> v = new Vertex<Integer>(1);
-		System.out.println(g.addVertex(v));
-		System.out.println(g.addVertex(v));
-		System.out.println(g.addVertex(new Vertex<Integer>(1)));
+		Graph<Integer, Integer> g = new Graph<Integer, Integer>(is_directed.DIRECTED);
+		Vertex<Integer> v1 = new Vertex<Integer>(1);
+		Vertex<Integer> v2 = new Vertex<Integer>(1);
+		System.out.println(g.addVertex(v1));
+		System.out.println(g.addVertex(v1));
+		System.out.println(g.addVertex(v2));
+		System.out.println(g.addEdge(new Edge<Integer, Integer>(v1, v2, 1)));
+		System.out.println(g.addEdge(new Edge<Integer, Integer>(v2, v1, 1)));
+		System.out.println(g.addVertex(v1));
 	}
 }
